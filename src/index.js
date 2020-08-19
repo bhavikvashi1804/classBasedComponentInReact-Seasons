df1c8415f6b54b87f50d605c8c49c6b2a4aa1579
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-08-19 16:39:26 +0530

    constructor to initialize the state

--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ import ReactDOM from "react-dom";
 import SeasonDisplay from "./components/SeasonDisplay";
 
 class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state={lat:null};
+  }
+  
+  //each Class component must have render method 
   render() {
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
