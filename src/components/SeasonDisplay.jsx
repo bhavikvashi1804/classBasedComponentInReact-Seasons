ac6667802596bff3f2175c75d4c91d2db2d7de7e
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-08-20 21:22:33 +0530

    display the icon

--- src/components/SeasonDisplay.jsx
+++ src/components/SeasonDisplay.jsx
@@ -10,8 +10,17 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth());
-  var text=season==="winter"?'Burr, it is chilly':'Let\'s hit the beach';
-  return <div>{text}</div>;
+  var text = season === "winter" ? "Burr, it is chilly" : "Let's hit the beach";
+  //<i class="snowflake icon"></i>
+  //<i class="sun icon"></i>
+  const icon = season === "winter" ? "snowflake" : "sun";
+  return (
+    <div>
+      <i className={`${icon} icon`} />
+      <h1>{text}</h1>
+      <i className={`${icon} icon`} />
+    </div>
+  );
 };
 
 export default SeasonDisplay;
