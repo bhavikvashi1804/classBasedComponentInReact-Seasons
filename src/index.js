730fa2a26eea15bc040c668ee10f9195d2f66313
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-08-20 21:53:44 +0530

    Loading Spinner when we are request for location and load the data

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import SeasonDisplay from "./components/SeasonDisplay";
+import Spinner from './components/Spinner';
 
 class App extends React.Component {
 
@@ -20,7 +21,7 @@ class App extends React.Component {
     } else if (!this.state.errorMsg && this.state.lat) {
       return <SeasonDisplay lat={this.state.lat}/>;
     } else {
-      return <div>Loading</div>;
+      return <Spinner />;
     }
   }
 }
