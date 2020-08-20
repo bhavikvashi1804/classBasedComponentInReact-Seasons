eb184f294dd843408730b7911ff7c2fde237fce4
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-08-20 22:05:37 +0530

    it is recommended that use the helper method

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import SeasonDisplay from "./components/SeasonDisplay";
-import Spinner from './components/Spinner';
+import Spinner from "./components/Spinner";
 
 class App extends React.Component {
-
-  state={ lat: null, errorMsg: "" };
+  state = { lat: null, errorMsg: "" };
 
   componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
@@ -15,14 +14,18 @@ class App extends React.Component {
     );
   }
 
-  render() {
+  renderContent() {
     if (this.state.errorMsg && !this.state.lat) {
       return <div>Error: {this.state.errorMsg}</div>;
     } else if (!this.state.errorMsg && this.state.lat) {
-      return <SeasonDisplay lat={this.state.lat}/>;
+      return <SeasonDisplay lat={this.state.lat} />;
     } else {
-      return <Spinner message="Please accept the location request"/>;
+      return <Spinner message="Please accept the location request" />;
     }
+  }
+
+  render() {
+    return (<div className="border red">{this.renderContent()}</div>);
   }
 }
 
