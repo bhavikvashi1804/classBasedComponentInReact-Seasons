065da2b6f272633dbfe85e6afd95603b032a7a9e
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-08-21 20:42:43 +0530

    Updates

---
+++ precache-manifest.f0c1ee5119bf2582bafac3cf6b3a2736.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4d9959d09d1dec4d438d5723118fe85",
+    "url": "/classBasedComponentInReact-Seasons/index.html"
+  },
+  {
+    "revision": "117cedfb1999471d3831",
+    "url": "/classBasedComponentInReact-Seasons/static/css/main.fd64f7b4.chunk.css"
+  },
+  {
+    "revision": "c10b42e9bee8656abb93",
+    "url": "/classBasedComponentInReact-Seasons/static/js/2.bdc50434.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/classBasedComponentInReact-Seasons/static/js/2.bdc50434.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "117cedfb1999471d3831",
+    "url": "/classBasedComponentInReact-Seasons/static/js/main.b8bf72fe.chunk.js"
+  },
+  {
+    "revision": "f363e213bbed0b405b3f",
+    "url": "/classBasedComponentInReact-Seasons/static/js/runtime-main.d8f78b56.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "//classBasedComponentInReact-Seasons/precache-manifest.81599a61d5bfd2b7de40d772367c5675.js"
+  "/classBasedComponentInReact-Seasons/precache-manifest.f0c1ee5119bf2582bafac3cf6b3a2736.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("//classBasedComponentInReact-Seasons/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/classBasedComponentInReact-Seasons/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
